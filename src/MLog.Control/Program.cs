1d482da841bc1b7609848d7def564e6e97eedd5c
lmf07120057 <821582374@qq.com>
2019-12-10 00:51:25 +0800

    初始化项目

---
+++ src/MLog.Control/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MLog.Control
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
